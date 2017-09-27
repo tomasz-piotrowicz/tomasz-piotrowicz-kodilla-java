43deb9e6aae90cba6cc42b52977e05cb49e843b5
tomasz-piotrowicz <pantomash@gmail.com>
2017-09-27 20:36:52 +0200

    Zadanie 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        String result = poemBeautifier.beautify("Lalalala", s -> s.toUpperCase());
+        System.out.println(result);
+
+        String result2 = poemBeautifier.beautify("Lalalalala", s -> s.concat("ABC"));
+        System.out.println(result2);
+
+        String result3 = poemBeautifier.beautify("Lalalalala", s -> s.substring(1,4));
+        System.out.println(result3);
+
+        String result4 = poemBeautifier.beautify("My name is Bond. James Bond", s -> s.replace("My", "His"));
+        System.out.println(result4);
+
+
+    }
+}
