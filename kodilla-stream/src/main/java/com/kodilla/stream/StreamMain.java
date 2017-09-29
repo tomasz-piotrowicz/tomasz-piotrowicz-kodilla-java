067a4a7f95304aab4816336a9cc71ac07ec4867e
tomasz-piotrowicz <pantomash@gmail.com>
2017-09-29 19:23:03 +0200

    Moduł 7.3 teoria

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,19 @@
 package com.kodilla.stream;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        String result = poemBeautifier.beautify("Lalalala", s -> s.toUpperCase());
-        System.out.println(result);
-
-        String result2 = poemBeautifier.beautify("Lalalalala", s -> s.concat("ABC"));
-        System.out.println(result2);
-
-        String result3 = poemBeautifier.beautify("Lalalalala", s -> s.substring(1,4));
-        System.out.println(result3);
-
-        String result4 = poemBeautifier.beautify("My name is Bond. James Bond", s -> s.replace("My", "His"));
-        System.out.println(result4);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
-    }
+        System.out.println(theResultStringOfBooks);
+        }
 }
