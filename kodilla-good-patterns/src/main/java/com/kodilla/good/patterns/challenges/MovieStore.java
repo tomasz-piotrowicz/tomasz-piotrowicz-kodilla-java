8a6eef9c6f94de248fc6e26fc801d83a24843ecf
tomasz-piotrowicz <pantomash@gmail.com>
2017-10-10 22:35:56 +0200

    Zadanie 9.1 poprawka

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class MovieStore {
@@ -32,9 +33,10 @@ public final class MovieStore {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        movieStore.getMovies().entrySet().stream()
-                .map(entry -> entry.getValue() + " ! ")
-                .forEach(System.out::println);
+        String getBooksTitles = movieStore.getMovies().entrySet().stream()
+                .map(entry -> entry.getValue().toString())
+                .collect(Collectors.joining("!", "", ""));
+        System.out.println(getBooksTitles);
     }
 
 }
