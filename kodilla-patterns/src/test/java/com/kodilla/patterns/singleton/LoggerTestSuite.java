44781a80ed7c2f144177d3da5a2d3da4cb8bf81c
tomasz-piotrowicz <pantomash@gmail.com>
2017-10-24 23:48:38 +0200

    Zadanie 11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @BeforeClass
+    public static void writeLog(){
+        Logger.getInstance().log("write log using class Logger");
+    }
+
+    @Test
+    public void testGetLastLog(){
+        //Given
+        //When
+        String logText = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("log using class Logger", logText);
+
+    }
+}
